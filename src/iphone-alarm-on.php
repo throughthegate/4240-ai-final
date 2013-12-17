a2bebe65c41381b2b94435094695b8a21424ce5f
Richmond Wong <ryw9@cornell.edu>
2013-12-17 18:18:43 -0500

    Add notification, app loading screens
    Support added for displaying notifications when links are clicked

--- src/iphone-alarm-on.php
+++ src/iphone-alarm-on.php
@@ -3,8 +3,9 @@ include_once('includes/firstLine.php'); //include this to check sessions.
 
 //all content goes into $content variable
 $content = "
-	<div id='alarm-off' class='iphone-app' >
+	<div id='alarm-on' class='iphone-app' >
 	</div>
+	<a href='iphone-alarm-off.php' id='alarm-toggle'> &nbsp; </a>
 	
 ";
 
--- src/iphoneTemplate.php
+++ src/iphoneTemplate.php
@@ -10,7 +10,7 @@
 	<link rel="stylesheet" type="text/css" href="styles/iphone.css">
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="functions.js"></script>
-	
+	<script src="loadergif.js"></script> <!-- loading gifs from http://preloaders.net/ -->
 	
 </head>
 
@@ -19,8 +19,25 @@
 		<div id='status-bar'>
 			<?php 
 			$time = date('g:i A');
-			print "<p class='text-center'>$time</p>"
+			print "
+				<p class='text-center'>
+					$time
+					
+				</p>
+				<p class='float-right' id='battery'>
+					<img src='img/icons/battery-01.png' />
+				</p>
+			";
 			?>
+		</div>
+		
+		<div id='notification-bar'>
+			<p id='notification-message'></p>
+		</div>
+		
+		<div id='load-screen'>
+			<div id="loaderImage" class='margin-auto'></div>
+			<p id='load-message'>App loading...</p>
 		</div>
 		
 		<?php
