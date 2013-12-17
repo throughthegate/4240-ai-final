7f8695ec6a36a6a8576026b376dbb5e2daf873a4
Richmond Wong <ryw9@cornell.edu>
2013-12-17 15:42:18 -0500

    Added generate_traits() to firstline.php

--- src/includes/firstLine.php
+++ src/includes/firstLine.php
@@ -1,20 +1,18 @@
 <?php
 session_start();
+include_once('ai.php');
 /*
-	Safety check - if no name is set, give it a default name. 
+	Safety check - if no name is set (1st time), give it a default name. 
 */
 if(!isset($_SESSION['name']) || empty($_SESSION['name'])){
 	$_SESSION['name'] = "John/Jane Doe";
+	//since this is the first visit, generate random traits
+	generate_traits();
 }
 
 /* Timezone default set to new york*/
 date_default_timezone_set('America/New_York');
 
-/*
-	Safety check on the 3 traits
-*/
-//include_once('ai.php');
-//TODO - generate 3 attributes
 
 
 ?>
--- src/iphone-alarm.php
+++ src/iphone-alarm.php
@@ -3,7 +3,9 @@ include_once('includes/firstLine.php'); //include this to check sessions.
 
 //all content goes into $content variable
 $content = "
-
+	<div id='alarm-off'>
+	
+	</div>
 ";
 
 //plugs the $content into the iphone template file.
