cf51e8c6eae699ce7f921e83d9003ad51a4554eb
Richmond Wong <ryw9@cornell.edu>
2013-12-17 21:24:29 -0500

    home button fixes?

--- src/index.php
+++ src/index.php
@@ -48,7 +48,7 @@ include_once('includes/firstLine.php'); //include this to check sessions.
 	
 	<!-- iphone div -->
 	<div id='iphone-div'>
-		<iframe id='iphone-iframe' src='iphone-home.php'  width='320' height='480' seamless='seamless' frameborder="0" allowtransparency="true" scrolling="no">Iframe of iphone</iframe>
+		<iframe id='iphone-iframe' name='iphone-iframe' src='iphone-home.php'  width='320' height='480' seamless='seamless' frameborder="0" allowtransparency="true" scrolling="no">Iframe of iphone</iframe>
 		<a id='home-button' class='text-center div-link' target='iphone-iframe' href="iphone-home.php">&nbsp;</a>
 	</div>
         
@@ -73,7 +73,7 @@ include_once('includes/firstLine.php'); //include this to check sessions.
         
         <!-- active area div - diagnostics div shows up when this div is moused over -->
         <div id='activate_d'>
-            <h1 id="hoverprompt"> >> Hover over to access options. <<</h1>
+            <h1 id="hoverprompt"> &gt;&gt; Hover over to access options. &lt;&lt</h1>
         </div>
 </body>
 
